ea647cd9a4447890cc965d64b46a294503eb0950
DahakaLab <Dahaka_Lab@mail.ru>
2017-09-16 22:40:46 +0300

    Add plugin for prod config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,9 +1,22 @@
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
+    devtool: 'source-map',
     entry: './src/index.js',
+    devServer: {
+        contentBase: './public'
+    },
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
-    }
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false
+            }
+        })
+    ]
 }
