0f6137d714386e75ab527d68da85dc1f2faf44e7
DahakaLab <Dahaka_Lab@mail.ru>
2017-09-17 14:30:54 +0300

    Add webpack hot and dev middleware plugin

--- src/app.js
+++ src/app.js
@@ -5,9 +5,11 @@ const app = Server.app();
 if (process.env.NODE_ENV !== 'production') {
     const webpack = require('webpack');
     const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware= require('webpack-hot-middleware');
     const config = require('../webpack.dev.config');
     const compiler = webpack(config);
 
+    app.use(webpackHotMiddleware(compiler));
     app.use(webpackDevMiddleware(compiler, {
         noInfo: true,
         publicPath: config.output.publicPath
