201e8292a8defd039f0076e1d8038d4d8946553b
DahakaLab <Dahaka_Lab@mail.ru>
2017-09-17 14:20:52 +0300

    Add express server

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const Server = require('./server');
+const port = (process.env.PORT || 3000);
+const app = Server.app();
+
+if (process.env.NODE_ENV !== 'production') {
+    const webpack = require('webpack');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const config = require('../webpack.dev.config');
+    const compiler = webpack(config);
+
+    app.use(webpackDevMiddleware(compiler, {
+        noInfo: true,
+        publicPath: config.output.publicPath
+    }));
+}
+
+app.listen(3000, function () {
+    console.log('App is the livein your laptop!\n');
+});
---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+module.exports = {
+    app: function () {
+        const app = express();
+        const indexPath = path.join(__dirname, '/../public/index.html');
+        const publicPath = express.static(path.join(__dirname, '../public'));
+
+        app.use('/public', publicPath);
+        app.get('/', function(_, res) {res.sendfile(indexPath)});
+
+        return app;
+    }
+};
