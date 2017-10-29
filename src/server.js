c787def48bdbf12b66d5b96ba4cc933c3e6768f6
DahakaLab <Dahaka_Lab@mail.ru>
2017-10-30 01:29:50 +0300

    End of test route

--- src/server.js
+++ src/server.js
@@ -5,11 +5,12 @@ module.exports = {
     app: function () {
         const app = express();
         const indexPath = path.join(__dirname, '/../public/index.html');
+        const  lolkekPath = path.join(__dirname, '/../public/views/lolkek.html');
         const publicPath = express.static(path.join(__dirname, '../public'));
 
         app.use('/public', publicPath);
-        app.get('/', function(_, res) {res.sendfile(indexPath)});
-        app.get('/lolkek', function (req, res) {res.send('Hello World!');});
+        app.get('/', function(_, res) {res.sendfile(indexPath);});
+        app.get('/lolkek', function (req, res) {res.sendfile(lolkekPath);});
 
         return app;
     }
