97a1d31172e1b6bfd5c1eab47c8b85ed7d3e7754
DahakaLab <Dahaka_Lab@mail.ru>
2017-11-01 00:52:20 +0300

    Remove comment of head title

--- src/data/nodes/titleNode.js
+++ src/data/nodes/titleNode.js
@@ -5,9 +5,6 @@ export function titleNode (){
     logoTitle.rel = 'shortcut icon';
     logoTitle.src = 'http://s1.iconbird.com/ico/2013/9/450/w256h2561380453904Leaf256x25632.png';
     logoTitle.type = 'image/x-icon';
-    logoTitle.classList = 'logo_header';
-
-//<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
 
     titleText = document.createElement('title');
     titleText.innerHTML = 'Чайный сервис';
