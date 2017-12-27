b21b1066fe9b2a4565f2425b6ab8fd73ade182a7
DahakaLab <Dahaka_Lab@mail.ru>
2017-12-27 21:42:54 +0300

    Raplace Hash hystory to Browser Router

--- src/pageContent/root.js
+++ src/pageContent/root.js
@@ -1,7 +1,7 @@
 import './../style/style.css';
 import 'babel-polyfill';
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { render } from 'react-dom';
 import { titleNode } from '../nodes/titleNode.js';
 import App from "../containers/app";
@@ -10,9 +10,9 @@ import App from "../containers/app";
     titleNode();
 
     render((
-        <HashRouter>
+        <BrowserRouter>
             <App/>
-        </HashRouter>
+        </BrowserRouter>
         ),document.getElementById('root')
     )
 })();
